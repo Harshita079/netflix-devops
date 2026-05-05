15debc55b868161b50cf574826159286711efb48
Harshita Pawar <harshu.shreyash64@gmail.com>
2026-05-05 21:25:47 +0530

    changes1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,20 @@
-#!/bin/bash
+stage('Deploy') {
+    steps {
+        sh '''
+        ssh -o StrictHostKeyChecking=no ec2-user@$APP_IP << EOF
 
-APP_IP=$1
+        docker stop $(docker ps -aq) || true
+        docker rm $(docker ps -aq) || true
+        docker rmi $(docker images -q) || true
 
-echo "Connecting to $APP_IP..."
+        rm -rf netflix-devops
+        git clone https://github.com/Harshita079/netflix-devops.git
 
-ssh -o StrictHostKeyChecking=no ec2-user@$APP_IP << 'EOF'
+        cd netflix-devops
+        docker build -t netflix-app .
+        docker run -d -p 80:80 netflix-app
 
-echo "Cleaning old setup..."
-docker stop netflix-container || true
-docker rm netflix-container || true
-docker rmi netflix-app || true
-
-echo "Removing old code..."
-rm -rf netflix-devops
-
-echo "Cloning fresh code..."
-git clone https://github.com/Harshita079/netflix-devops.git
-
-cd netflix-devops || exit
-
-echo "Building new Docker image..."
-docker build -t netflix-app .
-
-echo "Running new container..."
-docker run -d -p 80:80 --name netflix-container netflix-app
-
-echo "Deployment completed!"
-
-EOF
+        EOF
+        '''
+    }
+}
